95896ee09319cef21764fe920bc7adb619039bb2
AdamMalek <amalek999@o2.pl>
2017-12-24 23:02:22 +0100

    render fix + prefabs

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -12,38 +12,63 @@ public class GameManager : MonoBehaviour
     public GameObject twoWalls;
     public GameObject corner;
     public GameObject threeWalls;
+
+    private GameObject lvl1;
     void Start()
     {
         var g = new MazeGenerator();
-        var board = g.GenerateMaze(10, 10);
-        //var board = new Cell[2, 1];
+        var board0 = g.GenerateMaze(5, 5);
+        var board1 = g.GenerateMaze(5, 5);
+        //var board = new Cell[2,2];
         //board[0, 0] = new Cell(0, 0)
         //{
         //    x = 0,
         //    y = 0,
         //    visited = true,
-        //    walls = new[] { true, true, true, false }
+        //    walls = new[] { true, false, false, true }
         //};
         //board[1, 0] = new Cell(1, 0)
         //{
         //    visited = true,
-        //    walls = new[] { true, true, true, false }
+        //    walls = new[] { true, true, false, false }
         //};
         //board[0, 1] = new Cell(0, 1)
         //{
         //    visited = true,
-        //    walls = new[] { true, false, false, true }
+        //    walls = new[] { false, false, true, true }
         //};
         //board[1, 1] = new Cell(1, 1)
         //{
         //    visited = true,
-        //    walls = new[] { true, true, false, false }
+        //    walls = new[] { false, true, true, false }
         //};
 
-		var mazeRoot = new GameObject ("Maze");
+        var mazeRoot = new GameObject ("Maze");
+        var lvl0 = new GameObject("Level 0");
+        lvl1 = new GameObject("Level 1");
+
+        board0[0, 0].walls[0] = false;
+        board1[0, 0].walls[0] = false;
+
+        lvl0.transform.SetParent(mazeRoot.transform);
+        lvl1.transform.SetParent(mazeRoot.transform);
+        RenderLevel(lvl0, 0, board0);
+        RenderLevel(lvl1, 1, board1);
+        //corner up-rigth 0
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Z))
+        {
+        }
+    }
+
+    void RenderLevel(GameObject root, int level, Cell[,] board)
+    {
         foreach (var cell in board)
         {
-            var pos = new Vector3(2 * cell.x, 0, 2 * cell.y);
+            var pos = new Vector3(2 * cell.x, level*2 , -2 * cell.y);
             var rot = getRotation(cell);
             GameObject obj = null;
             switch (cell.walls.Count(x => x))
@@ -61,12 +86,11 @@ public class GameManager : MonoBehaviour
                     obj = threeWalls;
                     break;
             }
-			obj.name = "block[" + cell.x + ", " + cell.y + "]";
-			var inst = Instantiate(obj, pos, Quaternion.identity,mazeRoot.transform);
+            obj.name = "block[" + cell.x + ", " + cell.y + "]";
+            var inst = Instantiate(obj, pos, Quaternion.identity, root.transform);
             inst.transform.Rotate(rot);
             //inst.transform.Rotate(new Vector3(0,180,0));
         }
-        //corner up-rigth 0
     }
 
     private Vector3 getRotation(Cell cell)
